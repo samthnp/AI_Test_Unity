febbe67f97d413c04ae6079bfb7cda4dfd2260e9
Sam <thanaponpdm@gmail.com>
2022-01-21 00:37:07 -0500

    Add patrolling robot

--- AI_Test/Assets/Script/Guard.cs
+++ AI_Test/Assets/Script/Guard.cs
@@ -1,73 +1,29 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum GuardStates
-{
-    Patrolling, Waiting, Chasing
-}
-
 public class Guard : MonoBehaviour
 {
-    public GuardStates states;
 
-    public List<GameObject> wayPoints;
-    public int wayPointIndex;
+    public Material normalMaterial;
+    public Material emissiveMaterial;
 
-    public float patrolSpeed;
-
-    public float maxDuration = 5f;
-    public float durationDepletion = 1;
-    public float durationRegen = 0.5f;
-
-    private void Update()
+    public void EmissiveLightActivate()
     {
-        switch (states)
-        {
-            case GuardStates.Patrolling:
-                Patrol();
-                break;
-            case GuardStates.Waiting:
-                Wait();
-                break;
-            case GuardStates.Chasing:
-                Chase();
-                break;
-        }
-    }
+        MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
 
-    // States
-    void Patrol()
-    {
-        // move toward the current waypoint
-        // deplete patrol duration over time
-        
-        MoveTowardWaypoint();
-    }
+        Material oldMaterial = meshRenderer.material;
+        meshRenderer.material = emissiveMaterial;
 
-    void Wait()
-    {
-        
-    }
-
-    void Chase()
-    {
-        
     }
     
-    // State Functionalities
-
-    void MoveTowardWaypoint()
+    public void EmissiveLightDeactivate()
     {
-        transform.position = Vector3.MoveTowards(transform.position, wayPoints[wayPointIndex].transform.position, patrolSpeed * Time.deltaTime);
+        MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
+
+        Material oldMaterial = meshRenderer.material;
+        meshRenderer.material = normalMaterial;
+
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject == wayPoints[wayPointIndex]);
-        {
-            wayPointIndex = (wayPointIndex + 1) % wayPoints.Count;
-        }
-    }
 }
